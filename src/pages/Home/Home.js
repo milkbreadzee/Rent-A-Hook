d0dd37f07976870b825dec0643a2d75e8401494f
joel-roshan <joeljosephroshan@gmail.com>
2022-12-29 13:04:27 +0300

    boo2

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Home.css'
 const Home = () => {
   return (
-    <div>
+    <div className="main">
         <div className="cont">
             <div className="words">
                 <p>Tail Fishing Reel</p>
@@ -10,12 +10,12 @@ const Home = () => {
             <div>
               <button>Shop Now</button>
             </div>
-        <div className="cont2">
-            <div className="img1">
-                
-            </div>
-          </div>  
         </div>
+        
+        <div className="img1">
+                
+            
+        </div>  
     </div>
   )
 }
