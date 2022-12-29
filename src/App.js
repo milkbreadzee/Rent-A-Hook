7be135293a72cbdef5d4a98ed53dc68d98840266
joel-roshan <joeljosephroshan@gmail.com>
2022-12-29 15:03:06 +0300

    shop

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home/Home'
 import NewProduct from './pages/NewProduct/NewProduct';
+import Shop from './pages/Shop/Shop';
 
 function App() {
   return (
    <div>
-    <Header />
-    <Home />
-    <NewProduct />
-
+   <Shop />
    </div>
   );
 }
---
+++ src/pages/Shop/Shop.js
@@ -0,0 +1,67 @@
+import React from 'react'
+import Header from '../../components/Header';
+import './Shop.css'
+const Shop = () => {
+  return (
+    <div>
+        <Header />
+        <div className="products">
+            <h3 className='pro'>Product</h3>
+            {/* <p className='texthome'>Home</p> */}
+        </div>
+        <div className="items">
+
+            <div className="item1">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUXnhdI3LEPWrIltf3G-tEXa6uYEiKlbIPJQ&usqp=CAU" alt="" />
+                <div className="smalltext">
+                    name<br />price
+                </div>
+            </div>
+            <div className="item1">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUXnhdI3LEPWrIltf3G-tEXa6uYEiKlbIPJQ&usqp=CAU" alt="" />
+                <div className="smalltext">
+                    name<br />price
+                </div>
+            </div>
+            <div className="item1">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUXnhdI3LEPWrIltf3G-tEXa6uYEiKlbIPJQ&usqp=CAU" alt="" />
+                <div className="smalltext">
+                    name<br />price
+                </div>
+            </div>
+            <div className="item1">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUXnhdI3LEPWrIltf3G-tEXa6uYEiKlbIPJQ&usqp=CAU" alt="" />
+                <div className="smalltext">
+                    name<br />price
+                </div>
+            </div>
+            <div className="item1">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUXnhdI3LEPWrIltf3G-tEXa6uYEiKlbIPJQ&usqp=CAU" alt="" />
+                <div className="smalltext">
+                    name<br />price
+                </div>
+            </div>
+            <div className="item1">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUXnhdI3LEPWrIltf3G-tEXa6uYEiKlbIPJQ&usqp=CAU" alt="" />
+                <div className="smalltext">
+                    name<br />price
+                </div>
+            </div>
+            <div className="item1">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUXnhdI3LEPWrIltf3G-tEXa6uYEiKlbIPJQ&usqp=CAU" alt="" />
+                <div className="smalltext">
+                    name<br />price
+                </div>
+            </div>
+             <div className="item1">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUXnhdI3LEPWrIltf3G-tEXa6uYEiKlbIPJQ&usqp=CAU" alt="" />
+                <div className="smalltext">
+                    name<br />price
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Shop
