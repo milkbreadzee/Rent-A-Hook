e2cef908c57b1b617aae9cb17659806828ba4821
milkbreadzee <riaelisabeths@gmail.com>
2022-12-29 14:55:24 +0530

    home

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        <div className="cont">
+            <div className="words">
+                <p>hihihi</p>
+            </div>
+            <div className="item1img">
+                <img src="./item1.png"  alt="" />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
