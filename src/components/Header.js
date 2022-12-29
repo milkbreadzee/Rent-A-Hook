24498edb0c82db35bb2b34980d6da7d1e108b290
joel-roshan <joeljosephroshan@gmail.com>
2022-12-29 11:51:06 +0300

    navbar

---
+++ src/components/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './Header.css'
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BiUser } from 'react-icons/bi';
+import { AiOutlineHeart } from 'react-icons/ai';
+import { AiOutlineShoppingCart } from 'react-icons/ai';
+
+
+const Header = () => {
+  return (
+    <div class="main">
+        <nav class="navbar">
+
+            <div className="logo">
+            <h1>Rent-a-hook </h1>
+            </div>
+           
+            <p>home</p>
+            <p>shop</p>
+            <p>about</p>
+            <p>Pages</p>
+            <p>Blog</p>
+            <p>Contact</p>
+
+        </nav>
+        <div class="icons">
+        <AiOutlineSearch />
+        <BiUser />
+        <AiOutlineHeart />
+        <AiOutlineShoppingCart />
+        </div>
+    </div>
+  )
+}
+
+export default Header
